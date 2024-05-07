3902bb64413beacbd96ea0291e54456f25476ebe
Phiranath PO-NGERN <phukaow18@gmail.com>
2024-05-07 19:16:59 +0700

    change: class name Controller -> App

--- controller.py
+++ controller.py
@@ -2,7 +2,7 @@ from diabetes_view import DiabetesUI
 from diabetes_model import DiabetesModel
 
 
-class Controller:
+class App:
     """Controller for applying a design pattern in the future"""
     def __init__(self):
         self.view = DiabetesUI()
