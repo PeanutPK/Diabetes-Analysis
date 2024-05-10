c95d166bd98b16c4acdd10b57863516cfa60044d
Phiranath PO-NGERN <phukaow18@gmail.com>
2024-05-10 13:36:11 +0700

    delete unused import

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
 from application import DiabetesController
-from diabetes_view import DiabetesUI
-from diabetes_model import DiabetesModel
 
 if __name__ == '__main__':
-    model = DiabetesModel()
-    view = DiabetesUI()
     app = DiabetesController()
     app.run()
